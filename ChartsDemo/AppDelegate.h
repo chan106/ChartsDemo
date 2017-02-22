7415418b20a92b0851fa9f6bc7e2947983290b1e
Guo.JC <ji.chan@foxmail.com>
2017-02-22 19:50:32 +0800

    Initial Commit

---
+++ ChartsDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ChartsDemo
+//
+//  Created by coollang on 17/2/22.
+//  Copyright © 2017年 coollang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ChartsDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ChartsDemo
+//
+//  Created by coollang on 17/2/22.
+//  Copyright © 2017年 coollang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
